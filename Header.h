7ff81e73ccca3712bacb11849dbceee4c6f6ea88
Dawid Manczyk <mailnainpg@gmail.com>
2016-05-07 00:24:20 +0200

    Header - pierwsze 3 klasy

---
+++ Header.h
@@ -0,0 +1,31 @@
+#ifndef Header
+#define Header
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+
+class Objekt
+{
+	string nazwaKlasy;
+	int numerSali;
+	string nauczyciel;
+	string przedmiot;
+};
+
+class Godzina
+{
+	objekt * tablica;
+};
+
+
+class Plan
+{
+	Godzina ** tydzien;
+};
+
+
+
+#endif
