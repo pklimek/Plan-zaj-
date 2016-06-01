afc86f73f2ef6c4880a6f4d9194e2d6ff1692f35
jazdzyk <jazdzyk.jakub@gmail.com>
2016-06-01 17:22:31 +0200

    dokonano zmiany

--- Header.h
+++ Header.h
@@ -13,6 +13,7 @@ class Objekt
 	int numerSali;
 	string nauczyciel;
 	string przedmiot;
+	
 public:
 	Objekt (string l, int sala, string prowadzacy, string k);			//konstruktor wstawiaj¹cy dane do objektu "objekt"
 	void wyswietl (void);											//Funkcja sk³adowa wypisuj¹ca zawartoœæ objektu "Objekt"
